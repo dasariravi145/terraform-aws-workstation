ed56c53173d2e0de0ed813860da21eb84a5a3947
unknown <dasariravid145@gmail.com>
2026-08-12 23:11:38 +0530

    k8 cluster installation and k8 installation using automation for terraform

--- main.tf
+++ main.tf
@@ -53,3 +53,33 @@ resource "terraform_data" "cluster_destroy" {
              }
        }
 }
+
+
+resource "aws_security_group" "workstation" {
+  name        = "allow-all-workstation" # this is for AWS account
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow-all-workstation"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
